2dec0cb4e351e4da96e38f3fba88891e57fb8678
Deepscorn <vnms11@gmail.com>
2016-06-04 10:59:41 +0300

    must bind to AiContext, state is lightweight

--- Core/Utils/AI/AIContext.cs
+++ Core/Utils/AI/AIContext.cs
@@ -1,30 +1,44 @@
 // Copyright 2014 Tokarev Mikhail (also known as Deepscorn)
 // http://www.apache.org/licenses/LICENSE-2.0
+
+using System;
 using UniRx;
 
 namespace Utils.AI
 {
-	public class AiContext<TAiContext> where TAiContext : AiContext<TAiContext>
+	public class AiContext<TAiContext> : IDisposable where TAiContext : AiContext<TAiContext>
 	{
 		// State must not be assignable from client code. It is controlled from
 		// states themselves. Or maybe later it will be controlled from Context. But not client code
 		private AiState state;
+		private readonly BehaviorSubject<bool> stateEnterSubject = new BehaviorSubject<bool>(false);
 
 		public AiContext(AiState initialState)
 		{
 			state = initialState;
+			stateEnterSubject.OnNext(true);
 			state.EnterState(GetTemplateInstance());
 		}
 
+		// call that function to calculate AI state
 		public void Update()
 		{
 			state.Update(GetTemplateInstance());
 		}
 
+		public IObservable<T> UntilStateLeave<T>(IObservable<T> observable)
+		{
+			return observable.TakeUntil(stateEnterSubject.Where(enter => !enter));
+		}
+
+		// TODO bind to state update (?)
+
 		private void ChangeState(AiState newState)
 		{
+			stateEnterSubject.OnNext(false);
 			state.LeaveState(GetTemplateInstance());
 			state = newState;
+			stateEnterSubject.OnNext(true);
 			state.EnterState(GetTemplateInstance());
 		}
 
@@ -33,32 +47,29 @@ namespace Utils.AI
 			return (TAiContext) this;
 		}
 
+		public void Dispose()
+		{
+			stateEnterSubject.OnNext(false);
+		}
+
 		public class AiState
 		{
-			private readonly BehaviorSubject<bool> stateEnteredSubject = new BehaviorSubject<bool>(false);
 			// for future, real state. Can be used to group different AiState logics per state
 			//public virtual AIStateType Type { get; protected set; }
 
 			// Do something before we transition to this state
 			public virtual void EnterState(TAiContext context)
 			{
-				stateEnteredSubject.OnNext(true);
 			}
 
 			// Do something before we leave this state
 			public virtual void LeaveState(TAiContext context)
 			{
-				stateEnteredSubject.OnNext(false);
 			}
 
 			// Process the input - causes state actions, may cause state transition
 			public virtual void Update(TAiContext context)
 			{
-			}
-
-			protected IObservable<T> UntilLeave<T>(IObservable<T> observable)
-			{
-				return observable.TakeUntil(stateEnteredSubject.Where(entered => !entered));
 			}
 
 			protected virtual void ChangeState(TAiContext context, AiState newState)
